dde5aac040551c73038621dd6cf54aa33de21b00
Natalia <n.v@outlook.cl>
2017-05-15 22:17:34 -0400

    Agregando variables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+  //Creando variables
+  var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var contrasena = document.getElementById("input-password").value;
+  var tipoBici = document.querySelector("select").value;
+
+
+
+
 }
